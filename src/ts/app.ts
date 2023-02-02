cc1f1ce178e0a7986e20470c1336ebe640d9120d
Therese Hagwall <90thejag@skola.boras.se>
2023-02-02 14:00:30 +0100

    Inital commit

---
+++ src/ts/app.ts
@@ -0,0 +1,1 @@
+// import {person} from "./interface";
---
+++ src/ts/interface.ts
@@ -0,0 +1,21 @@
+
+
+export interface person{
+    name: string,
+    species: string,
+    gender: string,
+    house: string,
+    dateOfBirth: string,
+    yearOfBirth: number,
+    wizard: boolean,
+    ancestry: string,
+    eyeColour: string,
+    hairColour: string,
+    wand: {},
+    patronus: string,
+    hogwartsStudent: boolean,
+    hogwartsStaff: boolean,
+    actor: string,
+    alive: boolean,
+    image: string,
+}
