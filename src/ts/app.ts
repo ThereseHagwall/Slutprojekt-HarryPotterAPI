61d4794829b05b3ac172bd41c72799bf48a95718
Therese Hagwall <90thejag@skola.boras.se>
2023-02-07 09:15:19 +0100

    clean code

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,21 +1,18 @@
 // import {person} from "./interface";
 
-const baseUrl = 'https://legacy--api.herokuapp.com/api/v1/houses';
+const baseUrl = 'https://legacy--api.herokuapp.com/api/v1/houses/';
 //Get some elements from html
-const gryffindor = document.querySelector('#gryffindor') as HTMLImageElement;
-const slytherin = document.querySelector('#slytherin') as HTMLImageElement;
-const hufflepuff = document.querySelector('#hufflepuff') as HTMLImageElement;
-const ravewnclaw = document.querySelector('#ravenclaw') as HTMLImageElement;
+const allImages = [...document.querySelectorAll('.images')];
 const noFavorite = document.querySelector('#noFavorite') as HTMLButtonElement;
 const container = document.querySelector('.container') as HTMLDivElement;
-let card = document.createElement('section');
+const card = document.createElement('section');
 container.append(card);
 
-async function getHouseInfo(x:string){
+async function getHouseInfo(x:number){
     const response = await fetch(baseUrl+x);
     const data = await response.json();
     
-    let houseName = document.createElement('h2');
+    let houseName = document.createElement('h3');
     let founder = document.createElement('p');
     let animal = document.createElement('p');
     let traits = document.createElement('p');
@@ -28,41 +25,24 @@ async function getHouseInfo(x:string){
     container.append(card);
     card.append(houseName, founder, animal, traits, element, colors, ghost, commonRoom, img);
     
-    houseName.textContent = `${data.name}`;
-    founder.textContent = `Founder: ${data.founder}`;
-    animal.textContent = `Animal: ${data.animal}`;
-    traits.textContent = `Traits: ${data.traits}`;
-    element.textContent = `Element: ${data.element}`;
-    colors.textContent = `Colors: ${data.colors}`;
-    ghost.textContent = `Ghost: ${data.ghost}`;
-    commonRoom.textContent = `Common Room: ${data.common_room}`;
+    houseName.innerHTML = `${data.name}`;
+    founder.innerHTML = `Founder: ${data.founder}`;
+    animal.innerHTML = `Animal: ${data.animal}`;
+    traits.innerHTML = `Traits: ${data.traits}`;
+    element.innerHTML = `Element: ${data.element}`;
+    colors.innerHTML = `Colors: ${data.colors}`;
+    ghost.innerHTML = `Ghost: ${data.ghost}`;
+    commonRoom.innerHTML = `Common Room: ${data.common_room}`;
     img.src = data.image_url;        
 }
 
-//Listner of what choise the user make.
-gryffindor.addEventListener('click', (e) => {
-    e.preventDefault();
-    container.innerHTML = '';  
-    getHouseInfo('/1');
-})
-
-slytherin.addEventListener('click', (e) => {
-    e.preventDefault();
-    container.innerHTML = '';  
-    getHouseInfo('/2');
-})
-
-hufflepuff.addEventListener('click', (e) => {
-    e.preventDefault();
-    container.innerHTML = '';  
-    getHouseInfo('/3');
-})
-
-ravewnclaw.addEventListener('click', (e) => {
-    e.preventDefault();
-    container.innerHTML = '';  
-    getHouseInfo('/4');
-})
+for(let i = 1; i < allImages.length; i++) {
+    allImages[i].addEventListener('click', () => {
+        container.innerHTML = '';  
+        getHouseInfo(i);
+        
+    })
+}
 
 noFavorite.addEventListener('click', function(e){
     e.preventDefault();
