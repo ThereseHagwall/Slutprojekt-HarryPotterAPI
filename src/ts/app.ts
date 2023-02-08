4797ac22aceec60292775c0bbbdbff8c5e4db341
Therese Hagwall <90thejag@skola.boras.se>
2023-02-08 12:39:57 +0100

    Add functions and comments

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,12 +6,13 @@ const charUrl = 'https://hp-api.onrender.com/api/characters';
 const spellUrl = 'https://hp-api.onrender.com/api/spells';
 
 //Get some elements from html
+const body = document.querySelector('body') as HTMLBodyElement;
 const allImages = [...document.querySelectorAll('.images')];
 const noFavorite = document.querySelector('#noFavorite') as HTMLButtonElement;
 const container = document.querySelector('.container') as HTMLDivElement;
 const card = document.createElement('section');
-container.append(card);
 
+//A function to fetch the information about the house the user choose.
 async function getHouseInfo(x:number){
     const response = await fetch(baseUrl+x);
     const data = await response.json();
@@ -27,6 +28,7 @@ async function getHouseInfo(x:number){
     let img = document.createElement('img');
     
     container.append(card);
+    card.className = 'houseCard';
     card.append(houseName, founder, animal, traits, element, colors, ghost, commonRoom, img);
     
     houseName.innerHTML = `${data.name}`;
@@ -37,21 +39,83 @@ async function getHouseInfo(x:number){
     colors.innerHTML = `Colors: ${data.colors}`;
     ghost.innerHTML = `Ghost: ${data.ghost}`;
     commonRoom.innerHTML = `Common Room: ${data.common_room}`;
-    img.src = data.image_url;        
+    img.src = data.image_url;   
+    
+    if(x === 1){
+        card.style.backgroundColor = '#740001';
+        card.style.border = '10px solid #d3a625';
+        card.style.color = '#ffff'
+    }else if(x === 2){
+        card.style.backgroundColor = '#1a472a';
+        card.style.border = '10px solid #aaaaaa';
+        card.style.color = '#ffff'
+    }else if(x === 3){
+        card.style.backgroundColor = '#ecb939';
+        card.style.border = '10px solid #372e29';
+        card.style.color = '#372e29'
+    }else if(x === 4){
+        card.style.backgroundColor = '#0e1a40';
+        card.style.border = '10px solid #946b2d';
+        card.style.color = '#ffff'
+    }
 }
 
+//Array to store all housenames and Nohouse = hogwarts
+const houseArr: string[] = [
+    'Gryffindor',
+    'Slytherin',
+    'Hufflepuff',
+    'Ravenclaw',
+    'Hogwarts',
+]
+
+
+//Loop through all images to set a listner on everyone
 for(let i = 1; i < allImages.length; i++) {
     allImages[i].addEventListener('click', () => {
+        const text = document.createElement('p') as HTMLParagraphElement;
+        text.innerHTML = 'Change theme:';
+        body.append(text);
+
+        //Loop through the array to create radiobuttons and put listner to everyone.
+        for(let i = 0; i < houseArr.length; i++) {
+            const label = document.createElement('label');
+            label.innerHTML =  (`<input class="radiobutton" type="radio" name="themes" value="${houseArr[i]}">${houseArr[i]}`);
+            body.append(label);
+        }
+
         container.innerHTML = '';  
         getHouseInfo(i);
-        getSpellInfo();
-        getCharInfo();
+        getSpellInfo(6);
+        getCharInfo(8);
     })
 }
 
+let themesRadio = Array.from(document.querySelectorAll('.radiobutton'));   
+
+themesRadio.forEach(radioButton => {
+    radioButton.addEventListener('click', (event) => {
+        event.preventDefault();
+        console.log('changed');
+        
+    });
+});
+
+//A listner to de No favorite Button.
 noFavorite.addEventListener('click', function(e){
     e.preventDefault();
     container.innerHTML = '';  
+    const text = document.createElement('p') as HTMLParagraphElement;
+    text.innerHTML = 'Change theme:';
+    body.append(text);
+    
+    //Loop through the array to create radiobuttons and put listner to everyone.
+    for(let i = 0; i < houseArr.length; i++) {
+        const label = document.createElement('label');
+        label.innerHTML =  (`<input class="radiobutton" type="radio" name="themes" value="${houseArr[i]}">${houseArr[i]}`);
+        body.append(label);
+    }       
+    //A function to get all housenames and images 
     async function getAllHouseInfo(){
         const response = await fetch(`${baseUrl}`);
         const data = await response.json();
@@ -66,27 +130,37 @@ noFavorite.addEventListener('click', function(e){
             
             houseName.textContent = `${data[i].name}`;
             img.src = data[i].image_url; 
-        }       
+        }
     }
     getAllHouseInfo();
+    getSpellInfo(6);
+    getCharInfo(8);
 })
 
-async function getCharInfo(){
+//A function to fetch the information about characters
+async function getCharInfo(x:number){
     const response = await fetch(charUrl)
     const data = await response.json();
-
+    
     const charHeader = document.createElement('h3');
     const list = document.createElement('ul');
     const charCard = document.createElement('section');
     const more = document.createElement('p');
     more.innerHTML = 'View All';
     
+    more.addEventListener('click', (e) =>{
+        e.preventDefault();
+        container.innerHTML = '';
+        getCharInfo(data.length);
+    })
+    
     charCard.append(more);
-
+    
     charCard.className = 'charCard';
     charHeader.innerHTML = 'Characters';
     
-    for(let i = 0; i < 5; i++){
+    //Loop through 5 characters to write on the card
+    for(let i = 0; i < x; i++){
         let charName = document.createElement('p');
         charName.innerHTML += `<li>${data[i].name}</li>`;
         list.append(charName);
@@ -95,7 +169,8 @@ async function getCharInfo(){
     }
 }
 
-async function getSpellInfo(){
+//A function to fetch the information about spells
+async function getSpellInfo(x:number){
     const response = await fetch(spellUrl)
     const data = await response.json();
 
@@ -110,7 +185,8 @@ async function getSpellInfo(){
     spellCard.className ='spellCard';
     spellHeader.innerHTML = 'Spells';
     
-    for(let i = 0; i < 5; i++){
+    //Loop through 5 spells to write on the card
+    for(let i = 0; i < x; i++){
         let spellName = document.createElement('p');
         spellName.innerHTML += `<li>${data[i].name}</li> Description: ${data[i].description}`;
         
@@ -118,5 +194,12 @@ async function getSpellInfo(){
         spellCard.append(list, spellHeader);
         container.append(spellCard);
     }
+
+    more.addEventListener('click', (e) =>{
+        e.preventDefault();
+        container.innerHTML = '';
+        getSpellInfo(data.length);
+        more.innerHTML = '';
+})
 
 }
